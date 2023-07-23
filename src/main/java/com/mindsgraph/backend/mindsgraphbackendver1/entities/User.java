021cc5c3e9e7d6c71be1b0c8fd2206d8cc28d8f4
Ralph <raftsdev@gmail.com>
2023-07-23 13:28:21 -0400

    Feat: Add entity class User.

---
+++ src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/entities/User.java
@@ -0,0 +1,61 @@
+package com.mindsgraph.backend.mindsgraphbackendver1.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+
+    //Constructors
+    public User(){
+    };
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //getters and setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
