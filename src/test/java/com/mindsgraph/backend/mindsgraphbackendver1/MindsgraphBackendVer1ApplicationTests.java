92aa89480604fa05d6f73d5f660fbcaabe31bc14
Ralph <raftsdev@gmail.com>
2023-07-23 12:54:03 -0400

    Initial commit

---
+++ src/test/java/com/mindsgraph/backend/mindsgraphbackendver1/MindsgraphBackendVer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindsgraph.backend.mindsgraphbackendver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsgraphBackendVer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
