38ff5baabff09da9613db26796a2100c20128568
Ralph <raftsdev@gmail.com>
2023-07-24 12:12:58 -0400

    Fix: Add annotation @Table to fix error due difference name in database (users table).

--- src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/entities/User.java
+++ src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/entities/User.java
@@ -1,11 +1,9 @@
 package com.mindsgraph.backend.mindsgraphbackendver1.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
     @Id
     @GeneratedValue
