8eadb7dbb0210eaa9b98e9f1e9a56597b790c20c
Ralph <raftsdev@gmail.com>
2023-07-25 14:59:34 -0400

    Feat: Populate Rest Controller file.

--- src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/controllers/RestController.java
+++ src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/controllers/RestController.java
@@ -1,4 +1,28 @@
 package com.mindsgraph.backend.mindsgraphbackendver1.controllers;
 
+import com.mindsgraph.backend.mindsgraphbackendver1.entities.User;
+import com.mindsgraph.backend.mindsgraphbackendver1.repository.UserJpaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public RestController(UserJpaRepository userJpaRepository) {
+        super();
+        this.userJpaRepository = userJpaRepository;
+    }
+    private UserJpaRepository userJpaRepository;
+
+    @GetMapping("/rest-users")
+    public List<User> restUsers(){
+        return userJpaRepository.findAll();
+    }
 }
