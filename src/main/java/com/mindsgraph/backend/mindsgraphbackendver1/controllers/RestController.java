bbde51277e9c25adf4a2343b4f2c54f99850db75
Ralph <raftsdev@gmail.com>
2023-07-26 00:38:44 -0400

    Feat: Add retrieving REST data from Task table.

--- src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/controllers/RestController.java
+++ src/main/java/com/mindsgraph/backend/mindsgraphbackendver1/controllers/RestController.java
@@ -1,11 +1,12 @@
 package com.mindsgraph.backend.mindsgraphbackendver1.controllers;
 
+import com.mindsgraph.backend.mindsgraphbackendver1.entities.Task;
 import com.mindsgraph.backend.mindsgraphbackendver1.entities.User;
+import com.mindsgraph.backend.mindsgraphbackendver1.repository.TaskJpaRepository;
 import com.mindsgraph.backend.mindsgraphbackendver1.repository.UserJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -15,14 +16,21 @@ public class RestController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    public RestController(UserJpaRepository userJpaRepository) {
+    public RestController(UserJpaRepository userJpaRepository, TaskJpaRepository taskJpaRepository) {
         super();
         this.userJpaRepository = userJpaRepository;
+        this.taskJpaRepository = taskJpaRepository;
     }
     private UserJpaRepository userJpaRepository;
+    private TaskJpaRepository taskJpaRepository;
 
     @GetMapping("/rest-users")
     public List<User> restUsers(){
         return userJpaRepository.findAll();
+    }
+
+    @GetMapping("/rest-tasks")
+    public List<Task> restTasks(){
+        return taskJpaRepository.findAll();
     }
 }
